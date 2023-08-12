d0d58795a385b15ed099788afb7740062fdf681b
Simon Willison <swillison@gmail.com>
2023-08-12 16:16:08 -0700

    Load llm logs.db database on startup, if available

--- datasette_llm.py
+++ datasette_llm.py
@@ -1,7 +1,26 @@
 from datasette import hookimpl
+from datasette.database import Database
+import llm
 from llm.cli import cli as llm_cli
+import pathlib
 
 
 @hookimpl
 def register_commands(cli):
     cli.add_command(llm_cli, name="llm")
+
+
+@hookimpl
+def startup(datasette):
+    has_llm_db = False
+    try:
+        datasette.get_database("llm")
+        has_llm_db = True
+    except KeyError:
+        pass
+
+    config = datasette.plugin_config("llm") or {}
+    db_path = config.get("db_path") or (llm.user_dir() / "logs.db")
+    db_path = pathlib.Path(db_path)
+    if db_path.exists() and not has_llm_db:
+        datasette.add_database(Database(datasette, path=str(db_path)), name="llm")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,8 @@
 from datasette.cli import cli
 from click.testing import CliRunner
+import json
+import pytest
+import sqlite3
 
 
 def test_llm_commands():
@@ -7,3 +10,18 @@ def test_llm_commands():
     result = runner.invoke(cli, ["llm", "logs", "path"])
     assert result.exit_code == 0
     assert result.output.endswith("/logs.db\n")
+
+
+@pytest.mark.parametrize("logs_db_exists", (False, True))
+def test_startup_loads_llm_db_if_available(monkeypatch, tmpdir, logs_db_exists):
+    monkeypatch.setenv("LLM_USER_PATH", str(tmpdir))
+    if logs_db_exists:
+        path = str(tmpdir / "logs.db")
+        sqlite3.connect(path).execute("create table logs (id integer primary key)")
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--get", "/.json"])
+    info = json.loads(result.output)
+    if logs_db_exists:
+        assert "llm" in info
+    else:
+        assert "llm" not in info
